71553052c2d9058fb48fd0af8dae2f4deb9444cb
VeneraImamova <venus.imamova@gmail.com>
2022-08-14 00:28:45 +0700

    Zadacha 1 resh

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int randomNumber = new Random().Next(1, 5);
+System.Console.WriteLine(randomNumber);
+string inputNumber = Convert.ToString(randomNumber);
+
+if (inputNumber[1] == inputNumber[5] && inputNumber[2] == inputNumber[4])   Console.Write($"Число {inputNumber} является палиндромом");
+else Console.Write($"Число {inputNumber} НЕ является палиндромом");
