d5cffcb122cc3fc464ab696ee675c5accbddc985
VeneraImamova <venus.imamova@gmail.com>
2022-08-14 00:47:03 +0700

    Zadacha 1 done

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,12 +1,16 @@
-﻿// Задача 19
+﻿
+// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
 
-int randomNumber = new Random().Next(1, 5);
-System.Console.WriteLine(randomNumber);
-string inputNumber = Convert.ToString(randomNumber);
+Console.Write("Введите пятизначное число: ");
+string inputNumber = Console.ReadLine();
 
-if (inputNumber[1] == inputNumber[5] && inputNumber[2] == inputNumber[4])   Console.Write($"Число {inputNumber} является палиндромом");
-else Console.Write($"Число {inputNumber} НЕ является палиндромом");
+if (inputNumber.Length != 5)  Console.Write("Ошибка");
+else
+{
+    if (inputNumber[0] == inputNumber[4] && inputNumber[1] == inputNumber[3])   Console.Write($"Число {inputNumber} является палиндромом");
+    else Console.Write($"Число {inputNumber} НЕ является палиндромом");
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координату х1");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату х2");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y1");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y2");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z1");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z2");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(X1-X2, 2)+ Math.Pow(Y1-Y2, 2) + Math.Pow(Z1-Z2, 2));
+
+System.Console.WriteLine($"Расстояние равно: {Math.Round(d, 2)}");
+// Console.WriteLine($"Расстояние = {d:f2}");
